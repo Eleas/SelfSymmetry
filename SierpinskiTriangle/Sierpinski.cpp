a2e12df98cef1e51995f22ec7da8a65ed73a6ec7
Björn Paulsen <bjorn.paulsen@gmail.com>
2024-09-25 13:03:07 +0200

    add base sierpinski triangle code
    - unoptimized fractal code 
    - memoization would likely yield ridiculous speedup

---
+++ SierpinskiTriangle/Sierpinski.cpp
@@ -0,0 +1,180 @@
+#include "Sierpinski.h"
+#include "Point.h"
+
+Sierpinski::Sierpinski(olc::PixelGameEngine* pge)
+	: _height(0), _pge(pge) {
+	if (pge != nullptr) {
+		CreateMaxSizedTriangle();
+		InitValues();
+	}
+}
+
+/// <summary>
+/// Adds rotation in rightward direction.
+/// </summary>
+void Sierpinski::RotateCW() {
+	RotateTriangle(1.0);
+}
+
+/// <summary>
+/// Adds rotation in leftward direction.
+/// </summary>
+void Sierpinski::RotateCCW() {
+	RotateTriangle(-1.0);
+}
+
+void Sierpinski::RotateTriangle(const double angle) {
+	_top = RotatePoint(_top, angle);
+	_bottomLeft = RotatePoint(_bottomLeft, angle);
+	_bottomRight = RotatePoint(_bottomRight, angle);
+	_pge->Clear(olc::BLACK);
+	Draw();
+}
+
+Point Sierpinski::AsPoint(const olc::vi2d position) const
+{
+	return { static_cast<double>(position.x), static_cast<double>(position.y) };
+}
+
+/// <summary>
+/// Lets us set display values after object initialization.
+/// </summary>
+inline void Sierpinski::InitValues() {
+	_center = {
+		(_top.x + _bottomLeft.x + _bottomRight.x) / 3,
+		(_top.y + _bottomLeft.y + _bottomRight.y) / 3,
+	};
+}
+
+// Finds midpoint on imaginary line between two coordinates.
+inline constexpr olc::vi2d Sierpinski::Mid(const olc::vi2d p1, const olc::vi2d p2)
+{
+	return p1.lerp(p2, 0.5f);
+}
+
+inline constexpr Point Sierpinski::Mid(const Point p1, const Point p2)
+{
+	return { (p1.x + p2.x) / 2, (p1.y + p2.y) / 2 };
+}
+
+// Compute expensive square root function to a constant.
+inline constexpr float Sierpinski::TriangleRatio() {
+	return std::numbers::sqrt3_v<float> / 2.0f;
+}
+
+/// <summary>
+/// Fit the largest possible Sierpinski triangle into available space.
+/// </summary>
+/// <param name="margin">Surrounding blank-space margin, if desired.</param>
+inline void Sierpinski::CreateMaxSizedTriangle(const double margin) {
+	// Get screen dimensions minus margins
+	const double availableHeight = _pge->ScreenHeight() - 2 * margin;
+	const double availableWidth = _pge->ScreenWidth() - 2 * margin;
+
+	// In an equilateral triangle, height is (sqrt(3) / 2) * base
+	constexpr float heightToBaseRatio = TriangleRatio();
+
+	// Calculate the maximum base and height that fit within the available space
+	double maxBase = static_cast<double>(availableWidth);
+	double maxHeight = maxBase * heightToBaseRatio;
+
+	// Adjust base if height is too large to fit within screen
+	if (maxHeight > availableHeight) {
+		maxHeight = availableHeight;
+		maxBase = static_cast<int32_t>(maxHeight / heightToBaseRatio);
+	}
+
+	// Set the triangle's vertices
+	_top = { static_cast<double>(_pge->ScreenWidth() / 2), static_cast<double>(margin) }; // Top is centered horizontally
+	_bottomLeft = { (_pge->ScreenWidth() - maxBase) / 2, margin + maxHeight };
+	_bottomRight = { (_pge->ScreenWidth() + maxBase) / 2, margin + maxHeight };
+
+	// Store height value to end recursion and apply correct color.
+	_height = maxHeight;
+}
+
+/// <summary>
+/// Entrypoint for the DrawSierpinski method.
+/// </summary>
+void Sierpinski::Draw() {
+	DrawSierpinski(_top, _bottomLeft, _bottomRight, _height, olc::WHITE);
+}
+
+/// <summary>
+/// Recursive method to compute the Sierpinski triangle (split triangle into four triangles,
+/// then run the same method on the three corner triangles, ignoring the center one).
+/// </summary>
+/// <param name="top">Unambiguous.</param>
+/// <param name="bottomLeft">Unambiguous.</param>
+/// <param name="bottomRight">Unambiguous.</param>
+/// <param name="height">How tall the triangle is, or ought to be.</param>
+/// <param name="outlineColor">What color the outline should be in.</param>
+void Sierpinski::DrawSierpinski(const Point top, const Point bottomLeft, const Point bottomRight, const double height, const olc::Pixel outlineColor)
+{
+	auto fillColor = colorRamp[static_cast<int16_t>(height) % 16];
+	auto heightNextTriangle = height / 2; // Termination clause for recursion.
+
+	// Fill with background color.
+	_pge->FillTriangle(top.AsVi2d(), bottomLeft.AsVi2d(), bottomRight.AsVi2d(), fillColor);
+
+	// Then draw outlines.
+	_pge->DrawLine(top.AsVi2d(), bottomLeft.AsVi2d(), outlineColor);
+	_pge->DrawLine(top.AsVi2d(), bottomRight.AsVi2d(), outlineColor);
+	_pge->DrawLine(bottomLeft.AsVi2d(), bottomRight.AsVi2d(), outlineColor);
+
+	// New triangle.
+	Point midLeft = Mid(top, bottomLeft);
+	Point midRight = Mid(top, bottomRight);
+	Point midBottom = Mid(bottomLeft, bottomRight);
+	Point center = Mid(midLeft, midRight);
+
+	if (heightNextTriangle > 5) { // How to end recursion.
+		DrawSierpinski(top, midLeft, midRight, heightNextTriangle, outlineColor);
+		DrawSierpinski(midLeft, bottomLeft, midBottom, heightNextTriangle, outlineColor);
+		DrawSierpinski(midRight, midBottom, bottomRight, heightNextTriangle, outlineColor);
+	}
+}
+
+Point Sierpinski::RotatePoint(Point p, const double angleDegrees) {
+	double angleRadians = angleDegrees * PI / 180.0;
+	double s = sin(angleRadians);
+	double c = cos(angleRadians);
+
+	// Translate point back to origin
+	p.x -= _center.x;
+	p.y -= _center.y;
+
+	// Rotate point
+	double xnew = p.x * c - p.y * s;
+	double ynew = p.x * s + p.y * c;
+
+	// Translate point back
+	p.x = xnew + _center.x;
+	p.y = ynew + _center.y;
+
+	return p;
+}
+
+/*
+	Permission is hereby granted, free of charge, to any person obtaining a
+	copy of this software and associated documentation files (the “Software”),
+	to deal in the Software without restriction, including without limitation
+	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+	and/or sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included
+	in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS
+	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+	DEALINGS IN THE SOFTWARE.
+
+	--
+
+	It's a shame that US legislation saw fit to empower such terrible people.
+*/
---
+++ SierpinskiTriangle/Sierpinski.h
@@ -0,0 +1,81 @@
+#include <numbers>
+#include "olcPixelGameEngine.h"
+#include "Point.h"
+
+#pragma once
+class Sierpinski
+{
+public:
+	Sierpinski(olc::PixelGameEngine* pge = nullptr);
+	void InitValues();
+	void Draw();
+	void RotateCW();
+	void RotateCCW();
+
+private:
+	// Fields and constants.
+	const double PI = 3.14159266535;
+	Point _top;
+	Point _bottomLeft;
+	Point _bottomRight;
+	Point _center = { 0.0, 0.0 };
+	double _height;
+	olc::Pixel _outlineColor = olc::WHITE;
+	olc::PixelGameEngine* _pge;
+
+private:
+	// Color ramp.
+	olc::Pixel colorRamp[16] = {
+	olc::DARK_GREEN,
+	olc::VERY_DARK_GREY,
+	olc::RED,
+	olc::DARK_GREY,
+	olc::DARK_RED,
+	olc::VERY_DARK_RED,
+	olc::DARK_YELLOW,
+	olc::VERY_DARK_YELLOW,
+	olc::GREEN,
+	olc::VERY_DARK_GREEN,
+	olc::CYAN,
+	olc::DARK_CYAN,
+	olc::VERY_DARK_CYAN,
+	olc::BLUE,
+	olc::VERY_DARK_MAGENTA,
+	olc::DARK_CYAN
+	};
+
+private:
+	// Methods and functions
+	inline static constexpr olc::vi2d Mid(const olc::vi2d p1, const olc::vi2d p2);
+	inline static constexpr Point Mid(const Point p1, const Point p2);
+	void DrawSierpinski (const Point top, const Point bottomLeft, const Point bottomRight, const double height, const olc::Pixel outlineColor = olc::WHITE);
+	inline static constexpr float TriangleRatio();
+	void CreateMaxSizedTriangle(const double margin = 0);
+	void RotateTriangle(const double angle);
+	Point AsPoint(const olc::vi2d position) const;
+	Point RotatePoint(Point p, const double angleDegrees);
+};
+
+/*
+	Permission is hereby granted, free of charge, to any person obtaining a 
+	copy of this software and associated documentation files (the “Software”), 
+	to deal in the Software without restriction, including without limitation
+	the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+	and/or sell copies of the Software, and to permit persons to whom the 
+	Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included 
+	in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+	DEALINGS IN THE SOFTWARE.
+
+	--
+
+	It's a shame that US legislation saw fit to empower such terrible people.
+*/
