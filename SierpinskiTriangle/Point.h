f21880aaa10b6677fd00ec705a369e62e1ef3178
Björn Paulsen <bjorn.paulsen@gmail.com>
2024-09-25 13:06:56 +0200

    add constexpr to Point::AsVi2d

--- SierpinskiTriangle/Point.h
+++ SierpinskiTriangle/Point.h
@@ -4,7 +4,7 @@ struct Point
 	double x;
 	double y;
 
-	olc::vi2d AsVi2d() const {
+	inline constexpr olc::vi2d AsVi2d() const {
 		return { static_cast<int32_t>(x), static_cast<int32_t>(y) };
 	}
 };
@@ -12,7 +12,7 @@ struct Point
 
 /*
 	Permission is hereby granted, free of charge, to any person obtaining a
-	copy of this software and associated documentation files (the ìSoftwareî),
+	copy of this software and associated documentation files (the ‚ÄúSoftware‚Äù),
 	to deal in the Software without restriction, including without limitation
 	the rights to use, copy, modify, merge, publish, distribute, sublicense,
 	and/or sell copies of the Software, and to permit persons to whom the
@@ -21,7 +21,7 @@ struct Point
 	The above copyright notice and this permission notice shall be included
 	in all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED ìAS ISî, WITHOUT WARRANTY OF ANY KIND, EXPRESS
+	THE SOFTWARE IS PROVIDED ‚ÄúAS IS‚Äù, WITHOUT WARRANTY OF ANY KIND, EXPRESS
 	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
 	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
